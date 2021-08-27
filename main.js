0a95bfda1b030012aacbe4c11d02fd3c20831c00
harrish-07-super <84369483+harrish-07-super@users.noreply.github.com>
2021-08-27 18:41:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+tamil_song="";
+english_song="";
+
+leftWristX=0;
+rightWristX=0;
+
+leftWristY=0;
+rightWristY=0;
+
+function preload(){
+song1=loadSound("Rakita.mp3");
+
+song2=loadSound("beliver.mp3");
+}
+
+function setup(){
+canvas=createCanvas(600,400);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotposes);
+}
+
+function modelLoaded(){
+    console.log("Model is loaded!");
+}
+
+function gotposes(results){
+if(results.length>0){
+    console.log(results);
+leftWristX=results[0].pose.leftWrist.x;
+
+leftWristY=results[0].pose.leftWrist.y;
+
+rightWristX=results[0].pose.rightWrist.x;
+
+rightWristY=results[0].pose.rightWrist.y;
+
+console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+}
+}
+
+function draw(){
+image(video,0,0,600,400);
+}
